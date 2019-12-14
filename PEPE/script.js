5389b3846d79f3bbb028a72e589303e77b3aaa5d
skleinmann <57732514+skleinmann@users.noreply.github.com>
2019-12-13 21:28:18 -0600

    Add files via upload

---
+++ PEPE/script.js
@@ -0,0 +1,12 @@
+$("#doughnutChart").drawDoughnutChart([
+  { 
+    title: "Happy",
+    value : 4822,  
+    color: "#f3e32b" 
+  },
+  { 
+    title: "Sad",
+    value:  13339,   
+    color: "#35a8ff" 
+  }
+]);
